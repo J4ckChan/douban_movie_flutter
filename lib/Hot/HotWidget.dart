f7142d46885cfe150becab86520fd1a8370dabd1
JackChan829 <liang.chen829@gmail.com>
2019-07-14 20:55:32 +0800

    Tweak HotWidget Layout

--- lib/Hot/HotWidget.dart
+++ lib/Hot/HotWidget.dart
@@ -14,43 +14,46 @@ class HotWidgetState extends State<HotWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
-          height: 80,
-          padding: EdgeInsets.only(left: 8,right: 8),
-          alignment: Alignment.bottomCenter,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              FlatButton(
-                child:Row(
-                  children: <Widget>[
-                    Text(_curCity),
-                    Icon(Icons.arrow_drop_down),
-                  ],
+        Padding(
+          padding: EdgeInsets.only(top: 34),
+          child: Container(
+            height: 60,
+            padding: EdgeInsets.only(left: 8,right: 8),
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                FlatButton(
+                  child:Row(
+                    children: <Widget>[
+                      Text(_curCity,style: TextStyle(fontSize: 22,color: Colors.black54,),),
+                      Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
+                  onPressed: (){
+                    _jumpToSelectCtiy();
+                  },
                 ),
-                onPressed: (){
-                  _jumpToSelectCtiy();
-                },
-              ),
-              Expanded(
-                flex: 1,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16),
-                  decoration: InputDecoration(
-                    //prefixIcon: Icon(Icons.search),
-                    hintText: 'Search Movie & TV Series',
-                    filled: true,
-                    contentPadding: EdgeInsets.only(top: 8,bottom: 8),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                Expanded(
+                  flex: 1,
+                  child: TextField(
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 14),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Search Movie & TV Series',
+                      filled: true,
+                      contentPadding: EdgeInsets.only(top: 8,bottom: 8),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
-          )
+                )
+              ],
+            )
+          ),
         ),
         Expanded(
           child: DefaultTabController(
@@ -61,8 +64,8 @@ class HotWidgetState extends State<HotWidget> {
                   constraints: BoxConstraints.tightFor(height: 50),
                   child:TabBar(
                     tabs: <Widget>[
-                      Text('正在热映',style: TextStyle(color: Colors.lightBlue,fontSize: 16),),
-                      Text('即将上映',style: TextStyle(color: Colors.lightBlue,fontSize: 16),),
+                      Text('正在热映',style: TextStyle(color: Colors.blueGrey[800],fontSize: 20,fontWeight: FontWeight.w400),),
+                      Text('即将上映',style: TextStyle(color: Colors.blueGrey[800],fontSize: 20,fontWeight: FontWeight.w400),),
                     ],
                   ),
                 ),
