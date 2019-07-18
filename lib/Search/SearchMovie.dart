8d1a897778a343d4c8b913bfd461b881e5b0e003
JackChan829 <liang.chen829@gmail.com>
2019-07-18 23:07:14 +0800

    add 找电影 button in SearchMovie

--- lib/Search/SearchMovie.dart
+++ lib/Search/SearchMovie.dart
@@ -46,12 +46,64 @@ class _SearchMovieState extends State<SearchMovie> {
                           Column(
                             children: <Widget>[
                               Container(
-                                height: 120,
-                                color: Colors.grey,
+                                height: 140,
+                                color: Colors.pink,
                                 child: Center(
-                                  child: Text('moive',style: TextStyle(color: Colors.black),),
+                                  child: Text('ad',style: TextStyle(color: Colors.black),),
                                 ),
                               ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Expanded(
+                                    flex: 5,
+                                    child: Container(
+                                      height: 100,
+                                      color: Colors.blueGrey,
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(left: 16),
+                                        child: Row(
+                                          children: <Widget>[
+                                            Container(
+                                              height: 55,
+                                              width: 55,
+                                              decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                color: Colors.purple,
+                                              ),
+                                              child: Icon(Icons.list,size:40,color:Colors.white),
+                                            ),
+                                            Expanded(
+                                              child: Padding(
+                                                padding: const EdgeInsets.all(8.0),
+                                                child: Column(
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                                  children: <Widget>[
+                                                    Text("找电影",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                                                    Text('科幻/悬疑/剧情',style: TextStyle(fontSize: 14),)
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: const EdgeInsets.only(right: 8),
+                                              child: Icon(Icons.arrow_right),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Container(
+                                      height: 100,
+                                      color: Colors.cyan,
+                                    ),
+                                  )
+                                ],
+                              )
                             ],
                           ),
                           Center(
