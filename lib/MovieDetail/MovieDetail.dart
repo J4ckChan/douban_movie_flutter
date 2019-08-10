3ce7d5e33a348276212d69afbc23596d4c780664
JackChan829 <liang.chen829@gmail.com>
2019-08-10 23:31:20 +0800

    Change fetch moiveDetail method

--- lib/MovieDetail/MovieDetail.dart
+++ lib/MovieDetail/MovieDetail.dart
@@ -1,7 +1,8 @@
 import 'package:douban_movie_flutter/MovieDetail/MovieDetailData.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
+import 'dart:convert';
 
 class MovieDetail extends StatefulWidget {
   
@@ -18,43 +19,35 @@ class MovieDetail extends StatefulWidget {
 
 class _MovieDetailState extends State<MovieDetail> {
 
-  MovieDetailData movieDetailData;
-
-  @override
-  void initState() {
-    super.initState();
-    requestMovieDetail(widget.id);
-  }
-
-  Future requestMovieDetail(String id) async {
+  Future<MovieDetailData> fetchMovieDetail(http.Client client,String id) async {
     String url = 'https://api.douban.com/v2/movie/subject/'+id+'?apikey=0b2bdeda43b5688921839c8ecb20399b';
-    var response = await http.get(url);
+    final response = await client.get(url);
 
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      MovieDetailData movieDetailData = MovieDetailData.fromJson(jsonResponse);
-      setState(() {
-        this.movieDetailData = movieDetailData;
-      });
-    } else {
-      print('Request failed with State Code:${response.statusCode}');
-    }
+    return parseMoieDetail(response.body);
   }
+
+  MovieDetailData parseMoieDetail(String responseBody) {
+  final parsed = json.decode(responseBody);
+
+  return MovieDetailData.fromJson(parsed);
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('🎬电影'),),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 320,
-              color: Theme.of(context).accentColor,
-            )
-          ],
-        ),
-      ),
+      backgroundColor: Colors.redAccent[700],
+      appBar: AppBar(title: Text('电影'),backgroundColor: Colors.redAccent[700],),
+      body: FutureBuilder<MovieDetailData>(
+        future: fetchMovieDetail(http.Client(), widget.id),
+        builder: (context,snapshot){
+          if (snapshot.hasError) {
+            print(snapshot.error);
+          }
+          return snapshot.hasData 
+          ? Center(child: Text("Success"),)
+          :Center(child: CircularProgressIndicator(),);
+        },
+      )
     );
   }
 }
