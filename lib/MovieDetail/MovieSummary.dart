3c2cf9220791973b9f03dc01275e639cc177979f
JackChan829 <liang.chen829@gmail.com>
2019-08-13 22:46:11 +0800

    Tweak MovieSummary Layout

--- lib/MovieDetail/MovieSummary.dart
+++ lib/MovieDetail/MovieSummary.dart
@@ -13,37 +13,56 @@ class MovieSummary extends StatefulWidget {
 
 class _MovieSummaryState extends State<MovieSummary> {
 
-  int _maxLineOfSummary = 4;  
+  int _maxLineOfSummary = 4;
+  TextStyle style = TextStyle(fontSize: 18,color: Colors.white);
+
+  bool get isExceedMaxLines {
+    TextPainter _textPainter = TextPainter(
+      maxLines: _maxLineOfSummary,
+      text: TextSpan(
+        text: widget.summary,
+        style: this.style,
+      ),
+      textDirection: TextDirection.ltr,
+    )
+    ..layout(maxWidth: MediaQuery.of(context).size.width - 32,minWidth: MediaQuery.of(context).size.width - 32);
+    if (_textPainter.didExceedMaxLines) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  List<Widget> get sumaryList {
+    List<Widget> list = List();
+    list.add(Text(widget.summary,style:style,maxLines: _maxLineOfSummary,overflow: TextOverflow.ellipsis,));
+    
+    if (this.isExceedMaxLines) {
+      list.add(GestureDetector(
+        onTap: (){
+          setState(() {
+            this._maxLineOfSummary = 20;
+          });
+        },
+        child: Text('展开',style:TextStyle(fontSize: 18,color: Colors.white,decoration: TextDecoration.underline),));
+    }
+    return list;
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children:<Widget>[
         Padding(
-      padding: const EdgeInsets.only(left: 16,top: 8),
-      child: Text('剧情简介',style: TextStyle(fontSize: 24,color: Colors.white,fontWeight: FontWeight.bold),),
-    ),
+          padding: const EdgeInsets.only(left: 16,top: 8),
+          child: Text('剧情简介',style: TextStyle(fontSize: 24,color: Colors.white,fontWeight: FontWeight.bold),),
+        ),
         Padding(
             padding: const EdgeInsets.only(left: 16,right: 16,top: 8),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Text(
-                  widget.summary,
-                  style:TextStyle(fontSize: 18,color: Colors.white),
-                  maxLines: _maxLineOfSummary,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                GestureDetector(
-                  onTap: (){
-                    _maxLineOfSummary = 0;
-                    setState(() {
-                      _maxLineOfSummary = 8;
-                    });
-                  },
-                  child: Text('展开',style:TextStyle(fontSize: 18,color: Colors.white,decoration: TextDecoration.underline),)
-                ),
-              ],
+              children: sumaryList
             )
           ),
       ],
